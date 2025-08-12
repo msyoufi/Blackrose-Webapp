f51cd60b697304b488ee82bbbaed5b41215149c9
msyoufi <msyoufi.dev@gmail.com>
2025-08-13 00:37:58 +0200

    WIP: establich the perfume type definition.

---
+++ src/types/perfume.d.ts
@@ -0,0 +1,23 @@
+interface Perfume {
+  id: number,
+  brand: string,
+  name: string,
+  sex: PerfumeSex,
+  type: number,
+  size: number,
+  price: number,
+  in_stock: boolean,
+  image_url: string
+}
+
+type PerfumeSex = 'Men' | 'Women' | 'Unisex';
+
+type PerfumeType =
+  'Eau de Toilette'
+  | 'Eau de Parfum'
+  | 'Parfum'
+  | 'Elixir'
+  | 'Eau de Cologne'
+  | 'Eau Fraiche';
+
+type NewPerfume = Omit<Perfume, 'id'>;
