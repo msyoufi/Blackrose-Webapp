7e36a671b1ba41a43cdf74b99fe130cc634e07aa
msyoufi <msyoufi.dev@gmail.com>
2025-08-13 22:16:42 +0200

    WIP: create the header component.

---
+++ src/shared/components/header/header.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@mui/material';
+import { logout } from '../../services/auth.service';
+import { useSnackbar } from '../snackbar';
+import './header.scss';
+
+export default function Header() {
+  const snackbar = useSnackbar();
+
+  async function handleClick(): Promise<void> {
+    try {
+      await logout();
+      snackbar.show('Loged Out');
+
+    } catch (err: unknown) {
+      snackbar.show('Logout failed', 'error');
+    }
+  }
+
+  return (
+    <header>
+      <p></p>
+
+      <h1>Black Rose</h1>
+
+      <Button
+        className='auth-button'
+        variant='contained'
+        color='error'
+        onClick={handleClick}
+      >
+        logout
+      </Button>
+    </header>
+  );
+}
