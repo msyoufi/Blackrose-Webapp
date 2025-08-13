8266266195ed3b271a99e173e557a836ac035010
msyoufi <msyoufi.dev@gmail.com>
2025-08-13 14:23:13 +0200

    WIP: add create new perfume button to the perfumes header.

--- src/pages/perfumes/perfumes-grid-header/perfumes-grid-header.tsx
+++ src/pages/perfumes/perfumes-grid-header/perfumes-grid-header.tsx
@@ -1,7 +1,19 @@
+import { Button } from '@mui/material';
+import { usePerfumeForm } from '../perfume-form/perfume_form';
 import './perfumes-grid-header.scss';
 
 export default function PerfumesGridHeader() {
+  const perfumeForm = usePerfumeForm();
+
   return (
-    <p>Perfuems header</p>
+    <div className="perfume-grid-header">
+      <Button
+        size='small'
+        variant='contained'
+        onClick={() => perfumeForm.open()}
+      >
+        New Perfume +
+      </Button>
+    </div>
   );
 }
