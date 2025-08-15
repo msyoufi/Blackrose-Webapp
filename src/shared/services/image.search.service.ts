b7a045753315b2b344dad539c38866a056091271
msyoufi <msyoufi.dev@gmail.com>
2025-08-15 12:04:44 +0200

    WIP: implement the image search logic using goolge image search.

---
+++ src/shared/services/image.search.service.ts
@@ -0,0 +1,37 @@
+import { API_KEY, SEARCH_ENGINE_ID } from "../../secrets/search-engine.api-key";
+
+const baseURL = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ENGINE_ID}&q=`;
+
+export async function findImageUrl(perfume: Perfume): Promise<string> {
+  const { brand, name, concentration } = perfume;
+
+  const query = [brand, name, concentration].join(' - ');
+  const searchUrl = baseURL + encodeURIComponent(query);
+
+  const response = await fetch(searchUrl);
+  if (!response.ok) {
+    throw new Error('Unable to search for an image');
+  }
+
+  const data = (await response.json()).items;
+
+  if (!data || !data.length) {
+    throw new Error('No image found!');
+  }
+
+  for (const item of data) {
+    const imgUrl = item.pagemap?.hproduct?.[0]?.photo;
+    if (imgUrl) return imgUrl;
+  }
+
+  throw new Error('No image found!');
+}
+
+export async function downloadFileFromUrl(url: string): Promise<Blob> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Unable to download the file from URL');
+  }
+
+  return await response.blob();
+}
