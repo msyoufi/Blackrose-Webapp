fb3d6af9515d31ad15bfcbe39f39ed86226aec65
msyoufi <msyoufi.dev@gmail.com>
2025-08-22 15:39:58 +0200

    WIP: implement a method to reorder the perfumes.

--- src/shared/services/perfume.db.service.ts
+++ src/shared/services/perfume.db.service.ts
@@ -10,16 +10,40 @@ export async function updatePerfume(perfume: Perfume): Promise<void> {
   await updateDoc(getDocRef(perfume.id), perfumeData);
 }
 
-export async function updatePerfumesOrder(perfumes: Perfume[]): Promise<void> {
+export async function updatePerfumesOrder(orderedPerfumes: Perfume[]): Promise<void> {
   const batch = writeBatch(db);
 
-  perfumes.forEach((p, i) =>
+  orderedPerfumes.forEach((p, i) =>
     batch.update(getDocRef(p.id), { order: i + 1 })
   );
 
   await batch.commit();
 }
 
+export function reorderPerfumes(
+  allPerfumes: Perfume[],
+  movedPerfume: Perfume,
+  newOrder: number,
+  isDelete: boolean = false
+): Perfume[] {
+  const { id, collection } = movedPerfume;
+
+  // Order the perfume each in its own collection
+  const collPerfumes = allPerfumes.filter(p => p.collection === collection);
+
+  const currIndex = collPerfumes.findIndex(p => p.id === id);
+  if (currIndex < 0) throw new Error('Perfume not found');
+
+  const [moved] = collPerfumes.splice(currIndex, 1);
+
+  if (!isDelete) {
+    // clamp newIndex
+    const newIndex = Math.max(0, Math.min(newOrder - 1, collPerfumes.length));
+    collPerfumes.splice(newIndex, 0, moved);
+  }
+
+  return collPerfumes;
+}
 
 export async function deletePerfume(id: string): Promise<void> {
   await deleteDoc(getDocRef(id));
